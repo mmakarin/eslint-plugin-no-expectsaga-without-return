2bd5695c5542e8a724f6934fee2e0903162d439c
mmakarin <michael.makarin@gmail.com>
2020-08-17 13:29:14 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+module.exports = {
+    rules: {
+        'expectSaga-mandatory-return': {
+            meta: {
+                fixable: 'code'
+            },
+            create: function(context) {
+                return {
+                    ExpressionStatement(node) {
+                        if (node.expression.type === 'CallExpression' && node.expression.callee.name === 'expectSaga') {
+                            context.report({
+                            node,
+                            message: 'expectSaga without return',
+                
+                            fix: function(fixer) {
+                                return fixer.insertTextBefore(node, 'return ');
+                            }
+                            });
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
